0c7a275cd5962448f8edf9baad87a5b70bae31bd
CruelNightSky <138031715+CruelNightSky@users.noreply.github.com>
2023-06-29 15:32:11 +0700

    Fixed some nil pointer deref

--- src/main.go
+++ src/main.go
@@ -1,3 +1,7 @@
+// TODO
+// 1 HQ Storage is always 5x of normal storage
+// 2 Fix routh finding unction so it checks for existing node first before adding vertex
+
 package main
 
 import (
@@ -15,8 +19,8 @@ import (
 
 var (
 	t                 map[string]*table.Territory // loaded
-	ut 								map[string]*table.Territory // unloaded
-	st                int // second tick
+	ut                map[string]*table.Territory // unloaded
+	st                int                         // second tick
 	loadedTerritories = make(map[string]*table.Territory)
 )
 
@@ -162,105 +166,120 @@ func init() {
 }
 
 func main() {
-
+	if len(os.Args) < 2 {
+		log.Panicln("port not specified")
+	}
 	var port = os.Args[1]
-
 
 	if port == "" {
 		log.Panicln("$PORT must be set")
 	}
 
 	// start http server
-	http.HandleFunc("/eco", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/init", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Header().Add("Content-Type", "application/json")
+			w.Write([]byte(`{"code":405,"error":"method not allowed"}`))
+			return
+		}
+		// init territories specified in the request body
+		var bytes, err = io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-		} else if r.Method == "POST" {
-			switch r.URL.Path {
-			case "/eco/init":
-				// init territories specified in the request body
-				var bytes, err = io.ReadAll(r.Body)
-				if err != nil {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
+		var territories struct {
+			Territories []string `json:"territories"`
+			HQ          string   `json:"hq"`
+		}
+		json.Unmarshal(bytes, &territories)
 
-				var territories struct {
-					Territories []string `json:"territories"`
-					HQ          string   `json:"hq"`
-				}
-				json.Unmarshal(bytes, &territories)
+		log.Printf("Received %v", territories)
+		log.Println("initialising territories")
 
-				var hq = territories.HQ
+		var hq = territories.HQ
 
-				// if no hq provided
-				if hq == "" {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
+		// if no hq provided
+		if hq == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-				var zeroData = &table.TerritoryUpdateData{
-					Property: table.TerritoryProperty{
-						Upgrades: table.TerritoryPropertyUpgradeData{
-							Damage:  0,
-							Attack:  0,
-							Health:  0,
-							Defence: 0,
-						},
-						Bonuses: table.TerritoryPropertyBonusesData{
-							StrongerMinions:       0,
-							TowerMultiAttack:      0,
-							TowerAura:             0,
-							TowerVolley:           0,
-							LargerResourceStorage: 0,
-							LargerEmeraldStorage:  0,
-							EfficientResource:     0,
-							EfficientEmerald:      0,
-							ResourceRate:          0,
-							EmeraldRate:           0,
-						},
-						Tax: table.Tax{
-							Ally:   5,
-							Others: 5,
-						},
-						Border:       "Open",
-						TradingStyle: "Cheapest",
-						HQ:           false,
-					},
-				}
-				
-				for _, name := range territories.Territories {
-					// set all the territory properties to 0 or default
-					t[name].Set(*zeroData).SetAllyTax(5).SetOthersTax(60).OpenBorder().Cheapest()
-					loadedTerritories[name] = t[name]
-					if name == hq {
-						t[name].SetHQ()
-					}
-				}
+		var zeroData = table.TerritoryUpdateData{
+			Property: table.TerritoryProperty{
+				Upgrades: table.TerritoryPropertyUpgradeData{
+					Damage:  0,
+					Attack:  0,
+					Health:  0,
+					Defence: 0,
+				},
+				Bonuses: table.TerritoryPropertyBonusesData{
+					StrongerMinions:       0,
+					TowerMultiAttack:      0,
+					TowerAura:             0,
+					TowerVolley:           0,
+					LargerResourceStorage: 0,
+					LargerEmeraldStorage:  0,
+					EfficientResource:     0,
+					EfficientEmerald:      0,
+					ResourceRate:          0,
+					EmeraldRate:           0,
+				},
+				Tax: table.Tax{
+					Ally:   5,
+					Others: 5,
+				},
+				Border:       "Open",
+				TradingStyle: "Cheapest",
+				HQ:           false,
+			},
+		}
 
+		for _, name := range territories.Territories {
 
-
+			// set all the territory properties to 0 or default
+			t[name].Set(zeroData).SetAllyTax(5).SetOthersTax(60).OpenBorder().Cheapest()
+			loadedTerritories[name] = t[name]
+			if name == hq {
+				t[name].SetHQ()
 			}
 		}
+
+		var terrList = make(map[string]*table.Territory)
+		for _, name := range territories.Territories {
+			terrList[name] = t[name]
+		}
+
+		go getPathToHQ(t, hq)
+
+		log.Println("initialised territories")
+		log.Println(t["Ahmsord"])
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"code":200,"message":"initialised"}`))
 	})
 
-	http.ListenAndServe(port, nil)
+	http.ListenAndServe(":"+port, nil)
 
 	// run generateResource every 1s and resTick every 60s
 	var done = make(chan struct{})
 	var secTicker = time.NewTicker(time.Second)
 	var minTicker = time.NewTicker(time.Minute)
-	go func() {
+	go func(t map[string]*table.Territory) {
 		for {
 			select {
 			case <-secTicker.C:
 				generateResorce(t)
-			case <-minTicker.C:
-				resourceTick(t)
+				log.Println("tick")
 			case <-done:
+				secTicker.Stop()
+				minTicker.Stop()
 				return
 			}
 		}
-	}()
+	}(t)
 
 	fmt.Printf("test")
 }
@@ -273,7 +292,7 @@ func generateResorce(territories map[string]*table.Territory) {
 	// and if resource stored in the storage excees the capacity then the excess resource will be lost
 	// stoarge capacity is calculated like this
 	// cap = base cap * larger storage
-
+	log.Println(territories["Ahmsord"])
 	// emerald generation
 	for name, territory := range territories {
 		// calculate the resource production
@@ -283,12 +302,21 @@ func generateResorce(territories map[string]*table.Territory) {
 
 		// if the storage is full then do nothing
 		if float64(territory.Storage.Current.Emerald) >= emeraldStorage {
+			// send websocket message "A territory %s is producing more emeralds than it can store!"
 			continue
 		}
 
 		// if the storage is not full then generate the resource
+		// for hq
+		if float64(territory.Storage.Current.Emerald)+emeraldProduction <= emeraldStorage && !territory.Property.HQ {
+			currEms := float64((territories)[name].Storage.Current.Emerald)
+			currEms += emeraldProduction
+		} else if !territory.Property.HQ {
+			currEms := float64((territories)[name].Storage.Current.Emerald)
+			currEms += emeraldStorage
+		} else if float64(territory.Storage.Current.Emerald)+emeraldProduction <= emeraldStorage {
 
-		if float64(territory.Storage.Current.Emerald)+emeraldProduction <= emeraldStorage {
+			// normal terrs
 			currEms := float64((territories)[name].Storage.Current.Emerald)
 			currEms += emeraldProduction
 		} else {
@@ -297,6 +325,7 @@ func generateResorce(territories map[string]*table.Territory) {
 		}
 		fmt.Println(territories)
 	}
+
 	if st < 60 {
 		st++
 	} else {
@@ -317,33 +346,29 @@ func resourceTick(territories map[string]*table.Territory) {
 }
 
 func getPathToHQ(territories map[string]*table.Territory, name string) {
-	// get path to hq using djikstra, depending on the trading style
+	// get path to hq using dijkstra, depending on the trading style
 	// fastest  int
 	// will find the shortest path while ccheapest will find the shortest path with the least GLOBAL tax
 	// if the territory is the hq then return empty array
 
 	// connected nodes (territory) can be found at territories[name].TradingRoutes
 
+	log.Println(territories[name].TradingRoutes)
 	var dist int64 = 1
 
-	if territories[name].Property.HQ {
-		return 
-	}
-
+	log.Println("here")
 	var path []string
 	var graph = dijkstra.NewGraph()
-	var HQID *int
-	var currID = territories[name].ID
+	var HQID int
 
 	// find the id of hq
 	for _, territory := range territories {
 		if territory.Property.HQ {
-			*HQID = territory.ID
+			fmt.Println(territory.ID)
+			HQID = territory.ID
 			break
 		}
 	}
-
-	graph.AddVertex(currID)
 
 	for name := range territories {
 		// Add logic to compute the shortest path to HQ using Dijkstra's algorithm
@@ -352,29 +377,32 @@ func getPathToHQ(territories map[string]*table.Territory, name string) {
 
 		// add nearby nodes
 		for _, connection := range territories[name].TradingRoutes {
-
-			if territories[name].Property.TradingStyle == "Cheapest" && !territories[connection].Claim {
+			if territories[name].Property.TradingStyle == "Cheapest" {
+				connTerr := territories[connection]
+				graph.AddVertex(connTerr.ID)
 				dist = int64(territories[name].Property.Tax.Others)
-			} else if territories[name].Property.TradingStyle == "Cheapest" && territories[connection].Claim {
-				dist = 1
+				graph.AddArc(territories[name].ID, connTerr.ID, dist)
 			} else {
 				dist = 1
 			}
-
-			var conn = territories[connection]
-			graph.AddVertex(conn.ID)
-			graph.AddArc(territories[name].ID, conn.ID, dist)
 		}
-	}
 
-	var RouteToHQRaw, _ = graph.ShortestSafe(currID, *HQID)
-	for _, id := range RouteToHQRaw.Path {
-		for name := range territories {
-			if territories[name].ID == id {
-				path = append(path, name)
+		var RouteToHQRaw, err = graph.ShortestSafe(territories[name].ID, HQID)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println(territories[name].ID)
+		log.Println(RouteToHQRaw)
+
+		for _, id := range RouteToHQRaw.Path {
+			for name := range territories {
+				if territories[name].ID == id {
+					path = append(path, name)
+				}
 			}
 		}
-	}
 
-	territories[name].RouteToHQ = path
+		territories[name].RouteToHQ = path
+
+	}
 }
